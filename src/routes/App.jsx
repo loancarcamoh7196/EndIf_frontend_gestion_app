d4038e1fc1a9d438415f6f540bca34a8fa201830
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2022-06-03 10:47:00 -0400

    Agregar rutas y otras configuraciones

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,24 +1,35 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate  } from 'react-router-dom';
 
-import Layout from '@layouts/Main';
-import LoginLayout from '@layouts/Login';
-
+import NotFound from '@pages/NotFound';
 import Home from '@pages/Home';
 import Dashboard from '@pages/Dashboard';
 import Login from '@pages/Login';
+import Admin from '@pages/Admin';
+//Mantenedor Empresas
+import EmpresaIndex from '@pages/empresas/index';
+import EmpresaNew from '@pages/empresas/new';
 
 const App = () => {
   return (
     // <AppContext.Provider  value={useInitialState}>
     <Router>
       <Routes>
+        <Route path='/' element={<Home />} >
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='login' element={<Login />}  />
+
+          <Route path="admin" element={<Admin />}>
+            <Route path="empresas" element={<EmpresaIndex />} >
+              <Route path="new" element={<EmpresaNew />} />
+            </Route>
+            {/* <Route path="invoices" element={<Invoices />} /> */}
+          </Route>
+
         
-          <Route path='/' element={<Home />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-        
-        
-          <Route path='/login' element={<Login />}  />
+        </Route>
+        <Route path='*' element={<NotFound />} />
+        <Route  />
       </Routes>
     </Router>
     // </AppContext.Provider>
