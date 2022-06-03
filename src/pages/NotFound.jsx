29c6bd339a5712fbf487b13984fd34e82af7cb9b
Lorena Andrea Carcamo Herrera <lorenacarcamo6@gmail.com>
2022-06-03 10:46:06 -0400

    Agregar pagina 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import '@styles/404.scss';
 
 export default function NotFound(){
   return (
-		<div id='notfound'>
-			<div className='notfound'>
-				<div>
-					<div className='notfound-404'>
-						<h1>!</h1>
-					</div>
-					<h2>Error<br/>404</h2>
-				</div>
-				<p>
-					La página que estas buscando no existe o no se encuentra disponible. <br></br>
-					<Link to='/dashboard'>Volver</Link>
-				</p>
-			</div>
-		</div>
+		<Fragment>
+      <div id='notfound'>
+        <div className='notfound'>
+          <div>
+            <div className='notfound-404'>
+              <h1>!</h1>
+            </div>
+            <h2>Error<br/>404</h2>
+          </div>
+          <p>
+            La página que estas buscando no existe o no se encuentra disponible. <br></br>
+            <Link to='/'>Volver</Link>
+          </p>
+        </div>
+      </div>
+		</Fragment>
   );
 }
